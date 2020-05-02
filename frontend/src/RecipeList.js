2014bb904ec9cd05e15a194cef6f585702d983cd
carolinstuhr <stuhr.carolin@googlemail.com>
2020-05-02 11:40:15 +0200

    Adding Images

--- frontend/src/RecipeList.js
+++ frontend/src/RecipeList.js
@@ -8,6 +8,7 @@ export default function RecipeList() {
       <SectionStyled>
         {recipeData.map((recipe, index) => (
           <RecipeSectionStyled key={index}>
+            <ImageStyled src={recipe.image} alt="Recipe" />
             <TitleStyled>{recipe.title}</TitleStyled>
             <ListStyled>
               {recipe.tags.slice(0, 3).map((tag, index) => (
@@ -28,6 +29,38 @@ const SectionStyled = styled.main`
 const RecipeSectionStyled = styled.section`
   margin-bottom: 44px;
   margin-left: 16px;
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-template-rows: auto auto;
+`
+
+const ImageStyled = styled.img`
+  height: 80px;
+  width: 80px;
+  grid-row: 1 / 3;
+  border-radius: 12px;
+  object-fit: cover;
+  margin-right: 8px;
+`
+
+const TitleStyled = styled.h3`
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 22px;
+  text-transform: capitalize;
+  font-weight: 300;
+  margin-top: 4px;
+  margin-bottom: 8px;
+  width: 260px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
+
+const ListStyled = styled.ul`
+  list-style: none;
+  padding-left: 0;
+  margin-top: 4px;
+  margin-left: 0;
 `
 
 const ListItemsStyled = styled.li`
@@ -42,24 +75,4 @@ const ListItemsStyled = styled.li`
   font-size: 16px;
   text-transform: lowercase;
   color: #696660;
-`
-
-const ListStyled = styled.ul`
-  list-style: none;
-  padding-left: 0;
-  margin-top: 4px;
-  margin-left: 0;
-`
-
-const TitleStyled = styled.h3`
-  font-family: 'Josefin Sans', sans-serif;
-  font-size: 22px;
-  text-transform: capitalize;
-  font-weight: 300;
-  margin-top: 0;
-  margin-bottom: 8px;
-  width: 325px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
 `
