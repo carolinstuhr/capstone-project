deec11f65eef5c8fce7d1842ebc1880ea3fb5b66
carolinstuhr <stuhr.carolin@googlemail.com>
2020-05-04 20:53:12 +0200

    Adding input field tests

--- frontend/src/RecipeList.test.js
+++ frontend/src/RecipeList.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import RecipeList from './RecipeList'
 import { MemoryRouter } from 'react-router-dom'
 
@@ -11,4 +11,30 @@ test('rendering Recipe List', () => {
   )
   const linkElement = getByText(/Chocolate chip cookies/i)
   expect(linkElement).toBeInTheDocument()
+})
+
+test('test filter to search matching recipe', () => {
+  const { getByPlaceholderText, getByText } = render(
+    <MemoryRouter>
+      <RecipeList />
+    </MemoryRouter>
+  )
+  const input = getByPlaceholderText(/Search for recipes.../i)
+  fireEvent.change(input, { target: { value: 'porridge' } })
+  expect(getByText(/porridge/i)).toHaveTextContent(/porridge/i)
+})
+
+test('test filter with no match', () => {
+  const { getByPlaceholderText, getByText } = render(
+    <MemoryRouter>
+      <RecipeList />
+    </MemoryRouter>
+  )
+  const input = getByPlaceholderText(/Search for recipes.../i)
+  fireEvent.change(input, { target: { value: 'hdhdh' } })
+  expect(
+    getByText(
+      /Unfortunately, we did not find any recipe matching your search request./i
+    )
+  ).toHaveTextContent(/Unfortunately/i)
 })
