231e6d10e12522404dea98df7e50809e241ec04b
Caro <stuhr.carolin@googlemail.com>
2020-06-13 15:13:36 +0200

    Clean up code

--- src/RecipeDetails/RecipeDetails.js
+++ src/RecipeDetails/RecipeDetails.js
@@ -5,8 +5,7 @@ import { Link, useParams } from 'react-router-dom'
 import { db } from '../firebaseConfig'
 import ReturnIcon from './ReturnIcon'
 import RecipeRating from './RecipeRating'
-import { IoMdStar } from 'react-icons/io'
-import { IoIosCloseCircleOutline } from 'react-icons/io'
+import RecipeRatingWindow from './RecipeRatingWindow'
 
 export default function RecipeDetails({ user, recipes, previousPage }) {
   const params = useParams()
@@ -25,8 +24,6 @@ export default function RecipeDetails({ user, recipes, previousPage }) {
       recipe && Math.round(recipe.accumulatedRatings / recipe.numberOfRatings)
     )
   }, [recipe])
-
-  const [userRating, setUserRating] = useState(0)
 
   const [isFavourite, setIsFavourite] = useState(
     user && user.favourites.some((favourite) => favourite === recipe.id)
@@ -135,37 +132,11 @@ export default function RecipeDetails({ user, recipes, previousPage }) {
             )}
           </MainStyled>
           {isRatingWindowVisible && (
-            <RatingSection>
-              <CloseRatingIcon
-                onClick={() => setIsRatingWindowVisible(false)}
-              />
-              <RatingText>Please rate the recipe</RatingText>
-              <StarSection>
-                <StarIcon1
-                  userRating={userRating}
-                  onClick={() => setUserRating(1)}
-                />
-                <StarIcon2
-                  userRating={userRating}
-                  onClick={() => setUserRating(2)}
-                />
-                <StarIcon3
-                  userRating={userRating}
-                  onClick={() => setUserRating(3)}
-                />
-                <StarIcon4
-                  userRating={userRating}
-                  onClick={() => setUserRating(4)}
-                />
-                <StarIcon5
-                  userRating={userRating}
-                  onClick={() => setUserRating(5)}
-                />
-              </StarSection>
-              <RatingButton onClick={() => addUserRating(userRating)}>
-                Submit
-              </RatingButton>
-            </RatingSection>
+            <RecipeRatingWindow
+              setIsRatingWindowVisible={setIsRatingWindowVisible}
+              recipe={recipe}
+              setRecipeRating={setRecipeRating}
+            />
           )}
         </>
       )}
@@ -199,40 +170,6 @@ export default function RecipeDetails({ user, recipes, previousPage }) {
           .catch((err) =>
             alert('Something went wrong. Please try again later.', err)
           )
-  }
-
-  function addUserRating(rating) {
-    if (recipe.numberOfRatings === 0) {
-      db.collection('recipes')
-        .doc(recipe.id)
-        .update({ numberOfRatings: 1, accumulatedRatings: rating })
-        .then(() => {
-          setRecipeRating(rating)
-          setIsRatingWindowVisible(false)
-        })
-        .catch((err) =>
-          alert('Something went wrong. Please try again later.', err)
-        )
-    } else {
-      db.collection('recipes')
-        .doc(recipe.id)
-        .update({
-          numberOfRatings: recipe.numberOfRatings + 1,
-          accumulatedRatings: recipe.accumulatedRatings + rating,
-        })
-        .then(() => {
-          setRecipeRating(
-            Math.round(
-              (recipe.accumulatedRatings + rating) /
-                (recipe.numberOfRatings + 1)
-            )
-          )
-          setIsRatingWindowVisible(false)
-        })
-        .catch((err) =>
-          alert('Something went wrong. Please try again later.', err)
-        )
-    }
   }
 }
 
@@ -393,68 +330,4 @@ const InstructionsSection = styled.section`
 
 const ParagraphStyled = styled.p`
   margin-top: 4px;
-`
-
-const RatingSection = styled.section`
-  position: absolute;
-  z-index: 2;
-  background: var(--primary-background);
-  bottom: 2px;
-  width: 200px;
-  height: 150px;
-  top: 300px;
-  left: 88px;
-  border-radius: 4px;
-  border: 1px solid var(--tertiary);
-`
-
-const StarIcon = styled(IoMdStar)`
-  height: 30px;
-  width: 30px;
-`
-
-const StarIcon1 = styled(StarIcon)`
-  color: ${(props) => (props.userRating > 0 ? '#c82a1a' : 'white')};
-`
-const StarIcon2 = styled(StarIcon)`
-  color: ${(props) => (props.userRating > 1 ? '#c82a1a' : 'white')};
-`
-const StarIcon3 = styled(StarIcon)`
-  color: ${(props) => (props.userRating > 2 ? '#c82a1a' : 'white')};
-`
-const StarIcon4 = styled(StarIcon)`
-  color: ${(props) => (props.userRating > 3 ? '#c82a1a' : 'white')};
-`
-const StarIcon5 = styled(StarIcon)`
-  color: ${(props) => (props.userRating > 4 ? '#c82a1a' : 'white')};
-`
-
-const CloseRatingIcon = styled(IoIosCloseCircleOutline)`
-  position: absolute;
-  left: 178px;
-  height: 20px;
-  width: 20px;
-`
-
-const StarSection = styled.section`
-  display: flex;
-  justify-content: center;
-`
-
-const RatingText = styled.p`
-  text-align: center;
-  margin-top: 22px;
-  margin-bottom: 8px;
-  font-weight: 300;
-`
-
-const RatingButton = styled.button`
-  width: 60px;
-  padding: 4px;
-  margin-top: 18px;
-  margin-left: 70px;
-  border-radius: 4px;
-  font-size: 14px;
-  font-weight: 200;
-  background: white;
 `
---
+++ src/RecipeDetails/RecipeRatingWindow.js
@@ -0,0 +1,128 @@
+import React, { useState } from 'react'
+import styled from 'styled-components/macro'
+import { db } from '../firebaseConfig'
+import { IoMdStar } from 'react-icons/io'
+import { IoIosCloseCircleOutline } from 'react-icons/io'
+
+export default function RecipeRatingWindow({
+  recipe,
+  setIsRatingWindowVisible,
+  setRecipeRating,
+}) {
+  const [userRating, setUserRating] = useState(0)
+
+  return (
+    <RatingSection>
+      <CloseRatingIcon onClick={() => setIsRatingWindowVisible(false)} />
+      <RatingText>Please rate the recipe</RatingText>
+      <StarSection>
+        <StarIcon1 userRating={userRating} onClick={() => setUserRating(1)} />
+        <StarIcon2 userRating={userRating} onClick={() => setUserRating(2)} />
+        <StarIcon3 userRating={userRating} onClick={() => setUserRating(3)} />
+        <StarIcon4 userRating={userRating} onClick={() => setUserRating(4)} />
+        <StarIcon5 userRating={userRating} onClick={() => setUserRating(5)} />
+      </StarSection>
+      <RatingButton onClick={() => addUserRating(userRating)}>
+        Submit
+      </RatingButton>
+    </RatingSection>
+  )
+
+  function addUserRating(rating) {
+    if (recipe.numberOfRatings === 0) {
+      db.collection('recipes')
+        .doc(recipe.id)
+        .update({ numberOfRatings: 1, accumulatedRatings: rating })
+        .then(() => {
+          setRecipeRating(rating)
+          setIsRatingWindowVisible(false)
+        })
+        .catch((err) =>
+          alert('Something went wrong. Please try again later.', err)
+        )
+    } else {
+      db.collection('recipes')
+        .doc(recipe.id)
+        .update({
+          numberOfRatings: recipe.numberOfRatings + 1,
+          accumulatedRatings: recipe.accumulatedRatings + rating,
+        })
+        .then(() => {
+          setRecipeRating(
+            Math.round(
+              (recipe.accumulatedRatings + rating) /
+                (recipe.numberOfRatings + 1)
+            )
+          )
+          setIsRatingWindowVisible(false)
+        })
+        .catch((err) =>
+          alert('Something went wrong. Please try again later.', err)
+        )
+    }
+  }
+}
+
+const RatingSection = styled.section`
+  position: absolute;
+  z-index: 2;
+  background: var(--primary-background);
+  bottom: 2px;
+  width: 200px;
+  height: 150px;
+  top: 300px;
+  left: 88px;
+  border-radius: 4px;
+  border: 1px solid var(--tertiary);
+`
+
+const StarIcon = styled(IoMdStar)`
+  height: 30px;
+  width: 30px;
+`
+
+const StarIcon1 = styled(StarIcon)`
+  color: ${(props) => (props.userRating > 0 ? '#c82a1a' : 'white')};
+`
+const StarIcon2 = styled(StarIcon)`
+  color: ${(props) => (props.userRating > 1 ? '#c82a1a' : 'white')};
+`
+const StarIcon3 = styled(StarIcon)`
+  color: ${(props) => (props.userRating > 2 ? '#c82a1a' : 'white')};
+`
+const StarIcon4 = styled(StarIcon)`
+  color: ${(props) => (props.userRating > 3 ? '#c82a1a' : 'white')};
+`
+const StarIcon5 = styled(StarIcon)`
+  color: ${(props) => (props.userRating > 4 ? '#c82a1a' : 'white')};
+`
+
+const CloseRatingIcon = styled(IoIosCloseCircleOutline)`
+  position: absolute;
+  left: 178px;
+  height: 20px;
+  width: 20px;
+`
+
+const StarSection = styled.section`
+  display: flex;
+  justify-content: center;
+`
+
+const RatingText = styled.p`
+  text-align: center;
+  margin-top: 22px;
+  margin-bottom: 8px;
+  font-weight: 300;
+`
+
+const RatingButton = styled.button`
+  width: 60px;
+  padding: 4px;
+  margin-top: 18px;
+  margin-left: 70px;
+  border-radius: 4px;
+  font-size: 14px;
+  font-weight: 200;
+  background: white;
+`
